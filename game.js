aa5b04bf9c34dfc595e2e69d1be63789cd861d4a
Russell Rosario <rosariorussell@gmail.com>
2018-02-15 00:36:23 -0500

    started developing game functions

--- game.js
+++ game.js
@@ -1,20 +1,87 @@
-//  startgame
-//      array of words
-//          randomly pick word for game
-//      scoreboard array
-//      remainingLetters array for word
-//      event listener to take guess
-//      evaluate guess
-//          correct >> if event.key === value in remaining letters array
-//              loop for duplicate letters
-//              update scoreboard array
-//              evaluate win
-//          incorrect >> if event key !=== value in remaining letters array
-//              reduce guesses remaining
-//              game over?
-//      evaluate win/loss
-//          win >> scoreboard array does not contain any underscores
-//              congratulations, play again?
-//          loss >> 0 guesses remaining
-//              hangman game over
 
+
+var hangman = ["0000000000000",
+"0           1",
+"0           0",
+"0          1 1",
+"0           1",
+"0          324",
+"0         3 2 4",
+"0        3  2  4",
+"0          5 6",
+"0         5   6",
+"0        5     6",
+"0       5       6",
+"0",
+"0",
+"0",
+"0000000000000000000",]
+
+
+
+
+
+
+
+
+
+
+
+
+
+var words = ["one", "two", "three","four","five","six","seven","eight","nine","ten"];
+var lettersGuessedIncorrectly;
+var word;
+var blankedWordWithGuesses;
+var startGame = function () {
+    word = getRandomWord;
+    blankedWordWithGuesses = replaceWordWithBlanks(word.length);
+    updateIncorrectGuesses(blankedWordWithGuesses);
+}
+var getRandomWord = function (){
+    return words[Math.floor(Math.random()*words.length)];
+}
+var listenForKeyPress = function (){
+    document.onkeypress = handleKeypress;
+}
+var checkGuess = function (letter, word){
+    return word.indexOf(letter) > -1;
+}
+var replaceWordWithBlanks = function (length){
+    var str = '';
+    for (var i = 0; i < length; i++) {
+        str = str + " ";
+    }
+    return str.slice(0,-1);
+}
+var fillInGuesses = function (letter, word, blankedWordWithGuesses){
+
+}
+var handleKeyPress = function(event) {
+    var guess = event.key;
+}
+var handleCorrectGuess = function (){
+    
+}
+var handleIncorrectGuess = function (string){
+    
+}
+var handleKeypress = function (){
+    
+}
+var updateHangmanArt = function (){
+    
+}
+var updateIncorrectGuesses = function (){
+    var element = document.getElementById("correct-guesses");
+    element.innerHTML = string;
+}
+var updateCorrectGuesses = function (){
+    
+}
+var handleWin = function (){
+    
+}
+var handleLoss = function (){
+    
+}
